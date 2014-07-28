7f5b46534bff2e4ad7bf7fde4decf995909fbf70
franleplant <franleplant@gmail.com>
2014-07-28 15:34:31 -0300

    improve code readabilitty by introduccing a bridge variable i_u to avoid casting back and forth from int to uint

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 fn main() {
 
-	//let mut array_A = [31i, 41, 59, 26, 41, 58];
-	let mut array_A = [5i,2,4,6,1,3];
+	let mut array_A = [31i, 41, 59, 26, 41, 58];
+	//let mut array_A = [5i,2,4,6,1,3];
 	let mut i: int;
+
+	//utility variable. Uggly hack to avoid casting back and foward from int to uint
+	let mut i_u: uint; 
 	let mut key: int;
 
 
 	// It is not necessary to explicitly declare j
 	for j in range(1u, array_A.len()) { //arrays only work with uint (unsigned)
 		key = array_A[j];
-		i = j as int - 1;
+		i_u = j - 1;
+		i = i_u as int;
 
-		while i >= 0 && array_A[i as uint] > key {
-			array_A[i as uint + 1] = array_A[i as uint];
+		while i >= 0 && array_A[i_u] > key {
+			array_A[i_u + 1] = array_A[i_u];
+
 			i = i - 1;
+			i_u = i as uint;
 		};
-		array_A[i as uint+1]  = key;
+		array_A[i_u + 1]  = key;
 		
 	};
 
